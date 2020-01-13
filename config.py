a23bf8f8af81deca34717665ed4ff1b94d836957
avd9271 <avd9271@gmail.com>
2020-01-12 21:15:43 -0500

    removing comments. adding comment header

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+"""
+This file is for containing constants that are used project wide
+(such as sql connection string, query folder, etc.)
+
+"""
+
 # GENERIC
 import os
 from sys import platform
@@ -8,14 +14,3 @@ QUERY_FOLDER = ROOT_DIR + "/resources/queries/"
     
 # Postgres connection string:
 LOCAL_CONNECTION_STRING = "dbname='geolocator' user='postgres' host='localhost' password='postgres'"
-
-
-"""
-# will touch on this later. was trying to get all the sub packages on the same page
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-if platform == "win32":
-    QUERY_FOLDER = ROOT_DIR + "\\resources\\queries\\"
-else:
-    QUERY_FOLDER = ROOT_DIR + "/resources/queries/"
-"""
