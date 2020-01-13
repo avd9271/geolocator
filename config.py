2fbb7d84c779625271f8065b182628a86953c038
avd9271 <avd9271@gmail.com>
2020-01-12 21:11:35 -0500

    changing default password for sql server

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 QUERY_FOLDER = ROOT_DIR + "/resources/queries/"
     
 # Postgres connection string:
-LOCAL_CONNECTION_STRING = "dbname='geolocator' user='postgres' host='localhost' password='duvall9271'"
+LOCAL_CONNECTION_STRING = "dbname='geolocator' user='postgres' host='localhost' password='postgres'"
 
 
 """
